a023ffb9b59ade044c8a8c1011a0ff4ffd41f7f5
Akshay Narayan @ HP360 (MyOfficeThing) <okkhoy@gmail.com>
2023-02-17 11:29:31 +0800

    Add basic functionality to main

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -1,7 +1,50 @@
 package se.edu.inclass;
 
+import se.edu.inclass.data.DataManager;
+import se.edu.inclass.task.Deadline;
+import se.edu.inclass.task.Task;
+import se.edu.inclass.task.TaskNameComparator;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Main {
+
+    private TaskNameComparator taskNameComparator;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Task (stream) manager");
+        System.out.println("Welcome to Task (stream) manager\n");
+        DataManager dm = new DataManager("./data/data.txt");
+        ArrayList<Task> tasksData = dm.loadData();
+
+        System.out.println("Printing deadlines");
+        printDeadlines(tasksData);
+
+        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+
+    }
+
+    private static int countDeadlines(ArrayList<Task> tasksData) {
+        int count = 0;
+        for (Task t : tasksData) {
+            if (t instanceof Deadline) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void printData(ArrayList<Task> tasksData) {
+        for (Task t : tasksData) {
+            System.out.println(t);
+        }
+    }
+
+    public static void printDeadlines(ArrayList<Task> tasksData) {
+        for (Task t : tasksData) {
+            if (t instanceof Deadline) {
+                System.out.println(t);
+            }
+        }
     }
 }
